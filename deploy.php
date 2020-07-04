8ed5d60e98f7d6179f21a205a7ba10d8255de0b4
Alexey Mytsykov <alexmyt1@yandex.ru>
2020-07-04 17:01:52 +0400

    Update deployer config

--- deploy.php
+++ deploy.php
@@ -2,12 +2,19 @@
 namespace Deployer;
 
 require 'recipe/laravel.php';
+require 'recipe/rsync.php';
 
 // Project name
-set('application', 'my_project');
+set('application', getenv('CI_PROJECT_NAME'));
 
 // Project repository
 set('repository', 'https://gitlab.com/ukvp/ukvp.ru');
+
+set('ssh_multiplexing', true); // Speed up deployment
+
+set('rsync_src', function () {
+    return __DIR__; // If your project isn't in the root, you'll need to change this.
+});
 
 // [Optional] Allocate tty for git clone. Default value is false.
 //set('git_tty', true);
@@ -19,11 +26,32 @@ add('shared_dirs', []);
 // Writable dirs by web server.
 add('writable_dirs', []);
 
+// Configuring the rsync exclusions. 
+// You'll want to exclude anything that you don't want on the production server.  
+add('rsync', [
+    'exclude' => [
+        '.git',
+        '/.env',
+        '/storage/',
+        '/vendor/',
+        '/node_modules/',
+        '.gitlab-ci.yml',
+        'deploy.php',
+    ],
+]);
+
+// Set up a deployer task to copy secrets to the server. 
+// Since our secrets are stored in Gitlab, we can access them as env vars.
+task('deploy:secrets', function () {
+    file_put_contents(__DIR__ . '/.env', getenv('DOT_ENV'));
+    upload('.env', get('deploy_path') . '/shared');
+});
 
 // Hosts
 
 localhost()
-    ->set('deploy_path', '/var/www/ukvp.ru');
+    ->set('deploy_path', '/var/www/ukvp.ru')
+    ->stage('testing');
 
 // Tasks
 
@@ -33,6 +61,27 @@ task('build', function () {
 
 // [Optional] if deploy fails automatically unlock.
 after('deploy:failed', 'deploy:unlock');
+
+desc('Deploy the application');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'rsync', // Deploy code & built assets
+    'deploy:secrets', // Deploy secrets
+    'deploy:shared',
+    'deploy:vendors',
+    'deploy:writable',
+    'artisan:storage:link', // |
+    'artisan:view:cache',   // |
+    'artisan:config:cache', // | Laravel specific steps 
+    'artisan:optimize',     // |
+    'artisan:migrate',      // |
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+]);
 
 // Migrate database before symlink new release.
 
